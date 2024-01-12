b7ffd92dd7598cee6804f69ed91c1d7640ebf31a
Henrique Heron <henriqueheron1@gmail.com>
2024-01-12 00:04:32 -0300

    feat: add Search button for city searches

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import "./styles.css";
 
 document.addEventListener("DOMContentLoaded", () => {
   const citySearch = document.querySelector("#city");
+  const searchBtn = document.querySelector('#search-button');
 
   citySearch.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
       getWeather(citySearch.value);
     }
   });
+
+  searchBtn.addEventListener('click', getWeather(citySearch.value))
 
   const toggleMeasureBtn = document.querySelector("#toggle-measure");
   toggleMeasureBtn.addEventListener("click", (e) =>
